a5c03b30f2ae6f09b552f3aed7ca62d8b896cfce
Mahad <mahad.abd2003@gmail.com>
2023-06-21 22:43:25 +0400

    Change model to Faster R-CNN

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from detector import Detector
 
-MODEL_URL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
+# Faster R-CNN Model from TensorFlow's Object Detection Model Zoo
+MODEL_URL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_inception_resnet_v2_1024x1024_coco17_tpu-8.tar.gz"
 CLASSES_FILE = "coco.names"
-IMAGE_PATH = "tests/1.jpg"
 VIDEO_PATH = "tests/1.mp4"
 THRESHOLD = 0.5
 
@@ -10,6 +10,5 @@ detector = Detector()
 detector.read_classes(CLASSES_FILE)
 detector.download_model(MODEL_URL)
 detector.load_model()
-# detector.predict_image(IMAGE_PATH, THRESHOLD)
 detector.predict_video(VIDEO_PATH, THRESHOLD)
 
